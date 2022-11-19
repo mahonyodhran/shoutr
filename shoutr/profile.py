c3e0f5d83aa7584e0dc9edd428d59eb750138472
Odhran <mahonyodhran@gmail.com>
2022-11-19 00:22:16 +0000

    added route to view user profile by username - changed method names, one for id one for username

--- shoutr/profile.py
+++ shoutr/profile.py
@@ -7,7 +7,7 @@ from shoutr.db import get_db
 bp = Blueprint('profile', __name__, url_prefix='/profile')
 
 @bp.route('/<int:id>/', methods=('GET', 'POST'))
-def get_user(id):
+def get_user_by_id(id):
     user = get_db().execute(
         'SELECT id, username'
         ' FROM user'
@@ -17,5 +17,19 @@ def get_user(id):
 
     if user is None:
         abort(404, f"User {id} doesn't exist.")
+
+    return render_template('profile/view-profile.html', user=user)
+
+@bp.route('/<username>/', methods=('GET', 'POST'))
+def get_user_by_username(username):
+    user = get_db().execute(
+        'SELECT id, username'
+        ' FROM user'
+        ' WHERE username = ?',
+        (username,)
+    ).fetchone()
+
+    if user is None:
+        abort(404, f"User {username} doesn't exist.")
 
     return render_template('profile/view-profile.html', user=user)
