801dfbeec9f6f39f0bb39376600ddeb4a0bdb13a
Odhran <mahonyodhran@gmail.com>
2022-11-19 20:31:26 +0000

    black reformat

--- shoutr/profile.py
+++ shoutr/profile.py
@@ -1,38 +1,36 @@
-from flask import (
-    Blueprint, render_template
-)
+from flask import Blueprint, render_template
 from werkzeug.exceptions import abort
 from shoutr.db import get_db
 from shoutr.auth import login_required
 
-bp = Blueprint('profile', __name__, url_prefix='/profile')
+bp = Blueprint("profile", __name__, url_prefix="/profile")
 
-@bp.route('/<int:id>/', methods=('GET', 'POST'))
+
+@bp.route("/<int:id>/", methods=("GET", "POST"))
 @login_required
 def get_user_by_id(id):
-    user = get_db().execute(
-        'SELECT id, username'
-        ' FROM user'
-        ' WHERE id = ?',
-        (id,)
-    ).fetchone()
+    user = (
+        get_db()
+        .execute("SELECT id, username" " FROM user" " WHERE id = ?", (id,))
+        .fetchone()
+    )
 
     if user is None:
         abort(404, f"User {id} doesn't exist.")
 
-    return render_template('profile/view-profile.html', user=user)
+    return render_template("profile/view-profile.html", user=user)
 
-@bp.route('/<username>/', methods=('GET', 'POST'))
+
+@bp.route("/<username>/", methods=("GET", "POST"))
 @login_required
 def get_user_by_username(username):
-    user = get_db().execute(
-        'SELECT id, username'
-        ' FROM user'
-        ' WHERE username = ?',
-        (username,)
-    ).fetchone()
+    user = (
+        get_db()
+        .execute("SELECT id, username" " FROM user" " WHERE username = ?", (username,))
+        .fetchone()
+    )
 
     if user is None:
         abort(404, f"User {username} doesn't exist.")
 
-    return render_template('profile/view-profile.html', user=user)
+    return render_template("profile/view-profile.html", user=user)
